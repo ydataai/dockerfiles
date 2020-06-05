ed8868225347320a20d5f6ab2f82b0179136c067
Luís Portela Afonso <luis.portela@ydata.ai>
2020-06-05 10:40:02 +0100

    feat(h2oflow): support for H2O 💧 Flow and much more (#3)

--- data-science/build.sh
+++ data-science/build.sh
@@ -4,10 +4,13 @@ OLDPWD=$PWD
 
 cd $1
 
-build_args=$(for i in `cat build.args`; do out+="--build-arg $i " ; done; echo $out;out="")
-echo $build_args
+if [ -f "build.args" ]; then
+    build_args=$(for i in `cat build.args`; do out+="--build-arg $i " ; done; echo $out;out="")
+    echo "Running with argumens: $build_args"
+fi
+
 
 docker build --rm $build_args -t $2 .
-docker push $2
+# docker push $2
 
 cd $OLDPWD
