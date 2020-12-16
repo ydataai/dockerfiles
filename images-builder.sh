1eb3b73a1e0d6102120017828bd8d9ae8c127f13
Rafael Almeida <rafael.almeida@ydata.ai>
2020-12-16 16:33:59 +0000

    Drop JupyterLab 1 for JupyterLab 2

--- images-builder.sh
+++ images-builder.sh
@@ -1,6 +1,6 @@
-laboratories_images=(data-science/laboratories-base data-science/jupyterlab-1.2.15 \
-data-science/jupyterlab-1.2.15_tensorflow-1.15 data-science/jupyterlab-1.2.15_tensorflow-2.3 \
-data-science/jupyterlab-1.2.15_pytorch-1.7 data-science/h2oflow-3.32.0.2 data-science/ydata)
+laboratories_images=(data-science/laboratories-base data-science/jupyterlab-2.2.9 \
+data-science/jupyterlab-2.2.9_tensorflow-1.15 data-science/jupyterlab-2.2.9_tensorflow-2.3 \
+data-science/jupyterlab-2.2.9_pytorch-1.7 data-science/h2oflow-3.32.0.2 data-science/ydata)
 laboratories_images_types=(cpu gpu)
 
 for image in "${laboratories_images[@]}"
