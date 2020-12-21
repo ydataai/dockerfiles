d951323600c0c7dee0aaf8bd854d0d18f9e0aa1f
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2020-12-21 10:19:22 +0000

    fix: Add versions to images (#60)

--- images-builder.sh
+++ images-builder.sh
@@ -10,7 +10,7 @@ case $1 in
     ;;
 
   jupyterlab)
-      for image in data-science/jupyterlab data-science/jupyterlab_tensorflow-1.15 data-science/jupyterlab_tensorflow-2.3 data-science/jupyterlab_pytorch-1.7
+      for image in data-science/jupyterlab-1.2.15 data-science/jupyterlab-1.2.15_tensorflow-1.15 data-science/jupyterlab-1.2.15_tensorflow-2.3 data-science/jupyterlab-1.2.15_pytorch-1.7
       do
         for type in "${laboratories_images_types[@]}"
         do
@@ -23,8 +23,8 @@ case $1 in
   h2o)
       for type in "${laboratories_images_types[@]}"
       do
-          make build IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
-          make push IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
+          make build IMAGE="data-science/h2oflow-3.32.0.2" TYPE="$type" TAG="$2"
+          make push IMAGE="data-science/h2oflow-3.32.0.2" TYPE="$type" TAG="$2"
       done
     ;;
 
