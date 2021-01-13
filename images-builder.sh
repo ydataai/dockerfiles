756e8d023a38b89d2193374099baa31c9a6e57dc
Victória Olívia Araújo Vilas Boas <victoriaovilas+github@gmail.com>
2021-01-13 11:37:02 -0300

    feat: add git and debugger 1.x extension (#44)
    Co-authored-by: Luís Portela Afonso <luis.portela@ydata.ai>

--- images-builder.sh
+++ images-builder.sh
@@ -2,34 +2,37 @@ laboratories_images_types=(cpu gpu)
 
 case $1 in
   base)
-      for type in "${laboratories_images_types[@]}"
-      do
-          make build IMAGE="data-science/laboratories-base" TYPE="$type" TAG="$2"
-          make push IMAGE="data-science/laboratories-base" TYPE="$type" TAG="$2"
-      done
-    ;;
+    for type in "${laboratories_images_types[@]}"
+    do
+        make build IMAGE="data-science/laboratories-base" TYPE="$type" TAG="$2"
+        make push IMAGE="data-science/laboratories-base" TYPE="$type" TAG="$2"
+    done
+  ;;
 
   jupyterlab)
-      for image in data-science/jupyterlab data-science/jupyterlab_tensorflow-1.15 data-science/jupyterlab_tensorflow-2.3 data-science/jupyterlab_pytorch-1.7
-      do
-        for type in "${laboratories_images_types[@]}"
-        do
-            make build IMAGE="$image" TYPE="$type" TAG="$2"
-            make push IMAGE="$image" TYPE="$type" TAG="$2"
-        done
-      done
-    ;;
-
-  h2o)
+    for image in data-science/jupyterlab data-science/jupyterlab_tensorflow-1.15 data-science/jupyterlab_tensorflow-2.3 data-science/jupyterlab_pytorch-1.7
+    do
       for type in "${laboratories_images_types[@]}"
       do
-          make build IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
-          make push IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
+          make build IMAGE="$image" TYPE="$type" TAG="$2"
+          make push IMAGE="$image" TYPE="$type" TAG="$2"
       done
-    ;;
+    done
+  ;;
+
+  h2o)
+    for type in "${laboratories_images_types[@]}"
+    do
+        make build IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
+        make push IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
+    done
+  ;;
 
   ydata)
+    for type in "${laboratories_images_types[@]}"
+    do
       make build IMAGE="data-science/ydata" TAG="$2"
       make push IMAGE="data-science/ydata" TAG="$2"
-    ;;
+    done
+  ;;
 esac
