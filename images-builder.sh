f0c1f69c6c1ea09aada28a4abe1b9bca87a321e0
Victória Olívia Araújo Vilas Boas <victoriaovilas+github@gmail.com>
2021-01-14 20:04:17 -0300

    feat: Support R Studio (#79)

--- images-builder.sh
+++ images-builder.sh
@@ -17,6 +17,28 @@ case $1 in
     done
   ;;
 
+  rstudio)
+    for image in data-science/rstudio
+    do
+      for type in "${laboratories_images_types[@]}"
+      do
+          make build IMAGE="$image" TYPE="$type" TAG="$2"
+          make push IMAGE="$image" TYPE="$type" TAG="$2"
+      done
+    done
+  ;;
+
+  rstudio_bundles)
+    for image in data-science/rstudio_tensorflow data-science/rstudio_torch-1.7
+    do
+      for type in "${laboratories_images_types[@]}"
+      do
+          make build IMAGE="$image" TYPE="$type" TAG="$2"
+          make push IMAGE="$image" TYPE="$type" TAG="$2"
+      done
+    done
+  ;;
+
   jupyterlab_bundles)
     for image in data-science/jupyterlab_python_tensorflow-1.15 data-science/jupyterlab_python_tensorflow-2.3 data-science/jupyterlab_r_tensorflow-1.15 data-science/jupyterlab_r_tensorflow-2.3 data-science/jupyterlab_python_torch-1.7 data-science/jupyterlab_r_torch-1.7
     do
