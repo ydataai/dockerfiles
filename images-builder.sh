54aa041c1768af02b17c3bbd0e29f7c4b048a1d6
victoriaovilas <victoriaovilas+github@gmail.com>
2021-01-15 17:48:53 -0300

    fix: Add parallel jobs and upgrade R in ubuntu18

--- images-builder.sh
+++ images-builder.sh
@@ -9,38 +9,48 @@ case $1 in
     done
   ;;
 
-  jupyterlab)
+  jupyterlab_python)
     for type in "${laboratories_images_types[@]}"
     do
-      make build IMAGE="data-science/jupyterlab" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/jupyterlab" TYPE="$type" TAG="$2"
+      make build IMAGE="data-science/jupyterlab_python" TYPE="$type" TAG="$2"
+      make push IMAGE="data-science/jupyterlab_python" TYPE="$type" TAG="$2"
+    done
+  ;;
+
+  jupyterlab_r)
+    for type in "${laboratories_images_types[@]}"
+    do
+      make build IMAGE="data-science/jupyterlab_r" TYPE="$type" TAG="$2"
+      make push IMAGE="data-science/jupyterlab_r" TYPE="$type" TAG="$2"
     done
   ;;
 
   rstudio)
-    for image in data-science/rstudio
+    for type in "${laboratories_images_types[@]}"
     do
-      for type in "${laboratories_images_types[@]}"
-      do
-          make build IMAGE="$image" TYPE="$type" TAG="$2"
-          make push IMAGE="$image" TYPE="$type" TAG="$2"
-      done
+      make build IMAGE="data-science/rstudio" TYPE="$type" TAG="$2"
+      make push IMAGE="data-science/rstudio" TYPE="$type" TAG="$2"
     done
   ;;
 
-  rstudio_bundles)
-    for image in data-science/rstudio_tensorflow data-science/rstudio_torch-1.7
+  rstudio_tensorflow)
+    for type in "${laboratories_images_types[@]}"
     do
-      for type in "${laboratories_images_types[@]}"
-      do
-          make build IMAGE="$image" TYPE="$type" TAG="$2"
-          make push IMAGE="$image" TYPE="$type" TAG="$2"
-      done
+      make build IMAGE="data-science/rstudio_tensorflow" TYPE="$type" TAG="$2"
+      make push IMAGE="data-science/rstudio_tensorflow" TYPE="$type" TAG="$2"
     done
   ;;
 
-  jupyterlab_python_bundles)
-    for image in data-science/jupyterlab_python_tensorflow-1.15 data-science/jupyterlab_python_tensorflow-2.3 data-science/jupyterlab_python_torch-1.7
+  rstudio_torch)
+    for type in "${laboratories_images_types[@]}"
+    do
+      make build IMAGE="data-science/rstudio_torch-1.7" TYPE="$type" TAG="$2"
+      make push IMAGE="data-science/rstudio_torch-1.7" TYPE="$type" TAG="$2"
+    done
+  ;;
+
+  jupyterlab_python_tensorflow)
+    for image in data-science/jupyterlab_python_tensorflow-1.15 data-science/jupyterlab_python_tensorflow-2.3
     do
       for type in "${laboratories_images_types[@]}"
       do
@@ -50,14 +60,30 @@ case $1 in
     done
   ;;
 
-  jupyterlab_r_bundles)
-    for image in data-science/jupyterlab_r_tensorflow-1.15 data-science/jupyterlab_r_tensorflow-2.3 data-science/jupyterlab_r_torch-1.7
+  jupyterlab_r_tensorflow)
+    for image in data-science/jupyterlab_r_tensorflow-1.15 data-science/jupyterlab_r_tensorflow-2.3
     do
       for type in "${laboratories_images_types[@]}"
       do
         make build IMAGE="$image" TYPE="$type" TAG="$2"
         make push IMAGE="$image" TYPE="$type" TAG="$2"
       done
+    done
+  ;;
+
+  jupyterlab_python_torch)
+    for type in "${laboratories_images_types[@]}"
+    do
+      make build IMAGE="data-science/jupyterlab_python_torch-1.7" TYPE="$type" TAG="$2"
+      make push IMAGE="data-science/jupyterlab_python_torch-1.7" TYPE="$type" TAG="$2"
+    done
+  ;;
+
+  jupyterlab_r_torch)
+    for type in "${laboratories_images_types[@]}"
+    do
+      make build IMAGE="data-science/jupyterlab_r_torch-1.7" TYPE="$type" TAG="$2"
+      make push IMAGE="data-science/jupyterlab_r_torch-1.7" TYPE="$type" TAG="$2"
     done
   ;;
 
