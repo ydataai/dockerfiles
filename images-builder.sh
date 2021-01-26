1e165a0ceedcfb5ec73e78c2accbee97000beaff
Luís Portela Afonso <luis.portela@ydata.ai>
2021-01-26 23:11:39 +0000

    fix: use root user to install (#92)

--- images-builder.sh
+++ images-builder.sh
@@ -1,113 +1,73 @@
-laboratories_images_types=(cpu gpu)
 
 case $1 in
   base_python)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/laboratories-base" LANGUAGE="python" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/laboratories-base" LANGUAGE="python" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/laboratories-base" LANGUAGE="python" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/laboratories-base" LANGUAGE="python" TYPE="$2" TAG="$3"
   ;;
 
   base_r)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/laboratories-base" LANGUAGE="r" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/laboratories-base" LANGUAGE="r" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/laboratories-base" LANGUAGE="r" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/laboratories-base" LANGUAGE="r" TYPE="$2" TAG="$3"
   ;;
 
   jupyterlab_python)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/jupyterlab_python" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/jupyterlab_python" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/jupyterlab_python" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/jupyterlab_python" TYPE="$2" TAG="$3"
   ;;
 
   jupyterlab_r)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/jupyterlab_r" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/jupyterlab_r" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/jupyterlab_r" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/jupyterlab_r" TYPE="$2" TAG="$3"
   ;;
 
   rstudio)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/rstudio" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/rstudio" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/rstudio" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/rstudio" TYPE="$2" TAG="$3"
   ;;
 
   rstudio_tensorflow)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/rstudio_tensorflow" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/rstudio_tensorflow" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/rstudio_tensorflow" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/rstudio_tensorflow" TYPE="$2" TAG="$3"
   ;;
 
   rstudio_torch)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/rstudio_torch-1.7" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/rstudio_torch-1.7" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/rstudio_torch-1.7" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/rstudio_torch-1.7" TYPE="$2" TAG="$3"
   ;;
 
   jupyterlab_python_tensorflow)
     for image in data-science/jupyterlab_python_tensorflow-1.15 data-science/jupyterlab_python_tensorflow-2.3
     do
-      for type in "${laboratories_images_types[@]}"
-      do
-        make build IMAGE="$image" TYPE="$type" TAG="$2"
-        make push IMAGE="$image" TYPE="$type" TAG="$2"
-      done
+      make build IMAGE="$image" TYPE="$2" TAG="$3"
+      make push IMAGE="$image" TYPE="$2" TAG="$3"
     done
   ;;
 
   jupyterlab_r_tensorflow)
     for image in data-science/jupyterlab_r_tensorflow-1.15 data-science/jupyterlab_r_tensorflow-2.3
     do
-      for type in "${laboratories_images_types[@]}"
-      do
-        make build IMAGE="$image" TYPE="$type" TAG="$2"
-        make push IMAGE="$image" TYPE="$type" TAG="$2"
-      done
+      make build IMAGE="$image" TYPE="$2" TAG="$3"
+      make push IMAGE="$image" TYPE="$2" TAG="$3"
     done
   ;;
 
   jupyterlab_python_torch)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/jupyterlab_python_torch-1.7" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/jupyterlab_python_torch-1.7" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/jupyterlab_python_torch-1.7" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/jupyterlab_python_torch-1.7" TYPE="$2" TAG="$3"
   ;;
 
   jupyterlab_r_torch)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/jupyterlab_r_torch-1.7" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/jupyterlab_r_torch-1.7" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/jupyterlab_r_torch-1.7" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/jupyterlab_r_torch-1.7" TYPE="$2" TAG="$3"
   ;;
 
   h2o)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
-      make push IMAGE="data-science/h2oflow" TYPE="$type" TAG="$2"
-    done
+    make build IMAGE="data-science/h2oflow" TYPE="$2" TAG="$3"
+    make push IMAGE="data-science/h2oflow" TYPE="$2" TAG="$3"
   ;;
 
   ydata)
-    for type in "${laboratories_images_types[@]}"
-    do
-      make build IMAGE="data-science/ydata" TAG="$2"
-      make push IMAGE="data-science/ydata" TAG="$2"
-    done
+    make build IMAGE="data-science/ydata" TAG="$2"
+    make push IMAGE="data-science/ydata" TAG="$2"
   ;;
 esac
