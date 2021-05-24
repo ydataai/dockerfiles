cfb55cf77133c95f2a11e1131f3bea777d54bb04
Luís Portela Afonso <luis.portela@ydata.ai>
2021-05-24 16:14:53 +0100

    feat(elyra-runtimes): install new runtime images for all our bundles

---
+++ data-science/jupyterlab_python/entrypoint.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+elyra-metadata install runtimes \
+--name="ydata_pipelines" \
+--display_name="YData Pipelines" \
+--api_endpoint=http://ml-pipeline.kubeflow.svc.cluster.local:8888 \
+--user_namespace="$NAMESPACE" \
+--engine=Argo \
+--cos_endpoint=http://minio-service.kubeflow:9000 \
+--cos_username=minio \
+--cos_password=minio123 \
+--cos_bucket="elyra-$NAMESPACE" \
+--schema_name=kfp
+
+exec tini -g -- $@
