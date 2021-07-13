bf505e780c521e4e2bf8d02112394fa002ca8de5
Andre Vieira <andre.vieira@ydata.ai>
2021-07-13 23:28:42 +0000

    multiple fixes

--- docker-compose/build_all.sh
+++ docker-compose/build_all.sh
@@ -4,8 +4,7 @@ set -e
 echo "==> Start building images..."; echo "";
 for filename in *.yaml; do
     echo "===> Start building: $filename"; echo "";
-    docker-compose -f $filename build
-    echo ""; echo "===> Finish building: $filename"; echo "";
+    docker-compose -f $filename build >/dev/null
+    echo ""; echo "===> Finishing building: $filename"; echo "";
 done
 echo "==> Finish building images..."; echo "";
-
